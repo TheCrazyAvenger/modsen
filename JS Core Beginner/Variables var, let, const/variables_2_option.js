8d18db9d8518b665ba742ad030eb29b66a5a4c4b
Illia Pauliushchyk <ilyapavlushchik221@gmail.com>
2024-04-15 12:48:00 +0300

    Finish variables module

---
+++ JS Core Beginner/Variables var, let, const/variables_2_option.js
@@ -0,0 +1,10 @@
+var x = 'Первое объявление';
+var x = 'Второе объявление';
+
+console.log(x); // Выводит: 'Второе объявление'
+
+let y = 'Первое объявление';
+let y = 'Второе объявление'; // Ошибка: Identifier 'y' has already been declared
+
+const z = 'Первое объявление';
+const z = 'Второе объявление'; // Ошибка: Identifier 'z' has already been declared
